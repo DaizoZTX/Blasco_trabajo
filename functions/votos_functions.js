9d3f8cc75cd55f18baffc01e0f90f4df648f85f2
DaizoZTX <daitoztx@gmail.com>
2024-07-16 15:13:02 -0400

    3.0V UO

--- functions/votos_functions.js
+++ functions/votos_functions.js
@@ -1,33 +1,32 @@
 //Declaracion
 //Botones------------------------------------------------
 const botonVotar=document.getElementById('BotonVotar');
-const botonInicio=document.getElementById('BotonInicio');
+const botonInicio=document.getElementById('botonRegresar');
 
 const botonTarjeta = document.querySelectorAll(".tarjeta")
-
+//Sweet Alert
 for (i=0; i< botonTarjeta.length; i++){
     botonTarjeta[i].addEventListener('click', function()
 {
     Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
+        title: "Estas seguro de tu voto?",
+        text: "No podras revertirlo!",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonText: "Si, Estoy seguro!"
       }).then((result) => {
         if (result.isConfirmed) {
           Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
+            title: "Exito!",
+            text: "Su voto fue enviado con exito!",
             icon: "success"
           });
         }
       });
 })
 }
-
 //Checks-------------------------------------------------
 const checkCand1=document.getElementById('check1');
 const checkCand2=document.getElementById('check2');
@@ -35,3 +34,8 @@ const checkCand3=document.getElementById('check3');
 //Inicio-------------------------------------------------
 //EVENTOS
 //Click en el boton de volver al inicio
+botonInicio.addEventListener('click', function()
+{
+  alert("Sera dirigido al inicio de sesión!");
+  window.location.href="index.html"
+})
